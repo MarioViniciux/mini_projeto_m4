23457270252fe737668c68bfc0cfe5742b90faeb
Mario Vinicius <marioviniciuxcs@gmail.com>
2025-07-02 22:02:23 -0300

    :sparkles: adding cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import passwordRoutes from './routes/passwordRoutes.js';
 
@@ -7,6 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/passwords', passwordRoutes);
