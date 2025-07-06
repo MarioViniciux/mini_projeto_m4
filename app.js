2b526ddbc73d71b033dc4deb5362cc8df5394008
Mario Vinicius <marioviniciuxcs@gmail.com>
2025-07-06 06:22:37 -0300

    :bug: fix - corrigindo CORS para teste no frontend local

--- app.js
+++ app.js
@@ -7,10 +7,21 @@ import passwordRoutes from './routes/passwordRoutes.js';
 // Carrega as variáveis de ambiente do arquivo .env
 dotenv.config();
 
+const whiteList = ['http://localhost:3000', 'http://localhost:3001']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whiteList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Bloqueado por CORS'));
+        }
+    }
+}
+
 const app = express();
 
 // Habilita o CORS para permitir requisições de outros domínios
-app.use(cors());
+app.use(cors(corsOptions));
 // Permite o uso de JSON no corpo das requisições
 app.use(express.json());
 
